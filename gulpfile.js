8da56ec67b590a7d86bc516540712c74e57dded4
Huy Tran <gwyn@namtech.sg>
2022-07-04 23:20:03 +0700

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -6,34 +6,42 @@ const sass = require('gulp-sass')(require('sass'));
 var livereload = require('gulp-livereload');
 var concat = require('gulp-concat');
 
+var sassFolder = [
+    'src/assets/lib/**/*.css',
+    'src/assets/scss/**/*.scss',
+    'src/assets/scss/main.scss',
+];
+
+var destFolder = 'src/assets/main';
+
 // Compile Our Sass
 gulp.task('sass', function () {
-    return gulp.src([
-        'src/assets/lib/**/*.css',
-        'src/assets/scss/**/*.scss',
-        'src/assets/scss/main.scss',
-    ])
+    return gulp.src(sassFolder)
         .pipe(sass())
         .pipe(concat('main.css'))
         .pipe(sass())
-        .pipe(gulp.dest('src/assets/main/'))
+        .pipe(gulp.dest(destFolder))
         .pipe(livereload());
 });
 
 // Compile Our JavaScript
+var jsFolder = [
+    'src/assets/lib/**/*.js',
+    'src/assets/js/**/*',
+]
 gulp.task('js', function () {
-    return gulp.src('src/assets/js/**/*')
+    return gulp.src(jsFolder)
         .pipe(concat('main.js'))
-        .pipe(gulp.dest('src/assets/main'))
+        .pipe(gulp.dest(destFolder))
         .pipe(livereload());
 });
 
 // Watch Files For Changes
 gulp.task('watch', function () {
     livereload.listen();
-    gulp.watch('src/assets/lib/**/*', gulp.series('sass'));
-    gulp.watch('src/assets/scss/**/*', gulp.series('sass'));
-    gulp.watch('src/assets/js/**/*', gulp.series('js'));
+    // gulp.watch('src/assets/lib/**/*', gulp.series('sass'));
+    gulp.watch(sassFolder, gulp.series('sass'));
+    gulp.watch(jsFolder, gulp.series('js'));
 });
 
 // Default Task
